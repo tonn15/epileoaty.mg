2aaddccaf8f438229b06f2f852b87b13e51c2de9
AntonioRandrianarison <67898814+tonn15@users.noreply.github.com>
2025-07-25 22:35:54 +0200

    mise a jours

--- wp-content/themes/epileoaty/functions.php
+++ wp-content/themes/epileoaty/functions.php
@@ -3,23 +3,36 @@
  * EpileoAty Theme functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
  * @package EpileoAty
  * @since 1.0.0
  */
 
-/**
- * Define Constants
- */
-define( 'CHILD_THEME_EPILEOATY_VERSION', '1.0.0' );
+define('CHILD_THEME_EPILEOATY_VERSION', '1.0.0');
 
 /**
- * Enqueue styles
+ * ✅ Charger Tailwind via CDN AVANT les autres scripts
+ */
+function ajouter_tailwind_via_cdn() {
+    wp_enqueue_script(
+        'tailwindcdn',
+        'https://cdn.tailwindcss.com',
+        array(),
+        null,
+        false // Charge dans le <head>
+    );
+}
+add_action('wp_enqueue_scripts', 'ajouter_tailwind_via_cdn', 0); // priorité 0 = en tout premier
+
+/**
+ * ✅ Charger le style.css du thème enfant
  */
 function child_enqueue_styles() {
-
-	wp_enqueue_style( 'epileoaty-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_EPILEOATY_VERSION, 'all' );
-
+    wp_enqueue_style(
+        'epileoaty-theme-css',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('astra-theme-css'), // dépend de Astra
+        CHILD_THEME_EPILEOATY_VERSION,
+        'all'
+    );
 }
-
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
